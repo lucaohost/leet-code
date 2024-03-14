95d02f7b3de7726ded1fb3932f6d07f54a71616c
Lucas Reginatto de Lima <lucas.reginatto.de.lima@gmail.com>
2024-03-14 20:44:56 -0300

    Format cycle linked list challenge

--- 0006-cycle-linked-list/0006-cycle-linked-list.java
+++ 0006-cycle-linked-list/0006-cycle-linked-list.java
@@ -1,13 +1,17 @@
 // HackerRank provided the other necessary codes to run
-static boolean hasCycle(SinglyLinkedListNode head) {
+
+class HasCycle {
+
+  static boolean hasCycle(SinglyLinkedListNode head) {
     Map<SinglyLinkedListNode, Boolean> visitedNodes = new HashMap<>();
     SinglyLinkedListNode node = head;
     while (node != null) {
-        if (visitedNodes.get(node) != null) {
-            return true;
-        }
-        visitedNodes.put(node, true);
-        node = node.next;
+      if (visitedNodes.get(node) != null) {
+        return true;
+      }
+      visitedNodes.put(node, true);
+      node = node.next;
     }
     return false;
+  }
 }
