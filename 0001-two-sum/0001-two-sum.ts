43b1ec2fc3b2c85c6a86bfd9a1a388359fcd3e28
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2024-04-05 02:57:22 -0300

    Time: 172 ms (10.46%), Space: 52 MB (76.62%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i === j) {
+                continue;
+            }
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    // Se nenhum par for encontrado, retornar um array vazio
+    return [];
+}
