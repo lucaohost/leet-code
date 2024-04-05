5cacbfe86a973ed3ed55f5855499d254d62971ed
Lucas Reginatto <lucas.reginatto.de.lima@gmail.com>
2024-04-05 01:35:34 -0300

    Time: 53 ms (84.23%), Space: 51.5 MB (32.88%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.js
@@ -0,0 +1,19 @@
+var makeGood = function makeGood(s) {
+    const stack = [];
+    for (let char of s) {
+        if (stack.length > 0 && adjacentChars(char, stack[stack.length - 1])) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.join('');
+};
+
+function adjacentChars(char1, char2) {
+    if(char1.toUpperCase() === char1) {
+        return char1.toLowerCase() === char2;
+    } else {
+        return char1.toUpperCase() === char2;
+    }
+}
